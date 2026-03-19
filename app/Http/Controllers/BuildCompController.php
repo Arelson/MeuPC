09a6b2834623d25bc505001fb50d297f2ce749a8
Arlison <arlisong14@gmail.com>
2026-03-18 21:18:52 -0300

    Fazendo o uso das relações(build -> buildcomp)

--- app/Http/Controllers/BuildCompController.php
+++ app/Http/Controllers/BuildCompController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Services\BuildCompService;
 use App\Validators\BuildCompValidator;
 use App\Repositories\BuildCompRepository;
+use App\Services\ProdutoService;
 
 class BuildCompController extends Controller
 {
@@ -13,12 +14,14 @@ class BuildCompController extends Controller
     protected $buildCompService;
     protected $buildCompValidator;
     protected $buildCompRepository;
+    protected $produtoService;
 
-    public function __construct(BuildCompService $buildCompService, BuildCompValidator $buildCompValidator, BuildCompRepository $buildCompRepository)
+    public function __construct(BuildCompService $buildCompService, BuildCompValidator $buildCompValidator, BuildCompRepository $buildCompRepository, ProdutoService $produtoService)
     {
         $this->buildCompService = $buildCompService;
         $this->buildCompValidator = $buildCompValidator;
         $this->buildCompRepository = $buildCompRepository;
+        $this->produtoService = $produtoService;
     }
 
     public function index($id)
@@ -26,7 +29,26 @@ class BuildCompController extends Controller
        //return $this->buildCompService->listar();
 
        // Obtém os componentes relacionados à build específica usando o repositório
-       return $this->buildCompRepository->findWhere(['build_id' => $id]);
+       //return $this->buildCompRepository->findWhere(['build_id' => $id]);
+
+       $comp = $this->buildCompRepository->findWhere(['build_id' => $id]);
+
+       foreach ($comp as $c) {
+           $produto = $this->produtoService->obter($c->componente_id); // Carrega o produto relacionado
+           $c->produto_nome = $produto->nome; // Adiciona o nome do produto ao componente
+           $c->produto_preco = $produto->preco; // Adiciona o preço do produto ao componente
+           $c->produto_tipo = $produto->tipo; // Adiciona a descrição do produto ao componente
+           $c->subtotal = $produto->preco * $c->quantidade; // Calcula o subtotal para o componente
+       }
+
+       return response()->json(
+        [
+            'build_id' => $id,
+            'componentes' => $comp,
+            'subtotal' => $comp->sum('subtotal'), // Calcula o subtotal da build somando os subtotais dos componentes
+            //'total' => array_sum(array_column($comp->toArray(), 'subtotal')) // Calcula o total da build somando os subtotais dos componentes
+        ]
+       );
 
     }
 
@@ -40,9 +62,10 @@ class BuildCompController extends Controller
        return $this->buildCompService->create($request->all());
     }
 
-    public function show($id)
+    public function show($id, $componente_id)
     {
-        return $this->buildCompService->obter($id);
+        //return $this->buildCompService->obter($id);
+        return $this->buildCompRepository->findWhere(['build_id' => $id, 'componente_id' => $componente_id])->first();// esse fist é necessário porque o findWhere retorna uma coleção, mesmo que haja apenas um resultado, então precisamos pegar o primeiro item da coleção para obter o objeto BuildComp correto
     }
 
     public function edit($id)
@@ -50,13 +73,13 @@ class BuildCompController extends Controller
         return $this->buildCompService->obter($id);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id, $componente_id)
     {
-        return $this->buildCompService->update($id, $request->all());
+        return $this->buildCompService->update($request->all(), $componente_id);
     }
 
-    public function destroy($id)
+    public function destroy($id, $componente_id)
     {
-        return $this->buildCompService->destroy($id);
+        return $this->buildCompService->destroy($componente_id);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,15 @@ use App\Http\Controllers\BuildCompController;
 
 // Rota para obter os componentes de uma build específica
 Route::get('/builds/{id}/components', [BuildCompController::class, 'index']);
+// Rota para criar um novo componente para uma build específica
+Route::post('/builds/{id}/components', [BuildCompController::class, 'store']);
+// Rota para exibir um componente específico de uma build
+Route::get('/builds/{id}/components/{componente_id}', [BuildCompController::class, 'show']);
+// Rota para editar um componente específico de uma build
+Route::put('/builds/{id}/components/{componente_id}', [BuildCompController::class, 'update']);
+// Rota para excluir um componente específico de uma build
+Route::delete('/builds/{id}/components/{componente_id}', [BuildCompController::class, 'destroy']);
+
 
 // Rotas para API para evitar a certificação CSRF
 // Cria todas as rotas CRUD para os recursos User, Build e Produto
